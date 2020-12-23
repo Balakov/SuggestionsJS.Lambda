a4b66849d6501dd504adb4a9d53f1129db1fd473
Balakov <mike@mikestimpson.com>
2020-12-23 17:04:21 +0000

    Add GetShowID method

---
+++ Methods/GetShowID.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.DynamoDBv2;
+using System.Threading.Tasks;
+
+namespace SuggestionsJS
+{
+    public static partial class Methods
+    {
+        public class GetShowIDResponse
+        {
+            public string ShowID { get; set; }
+        }
+
+        // { "function": "getShowID" }
+
+        public static async Task<GetShowIDResponse> GetShowIDAsync()
+        {
+            return new GetShowIDResponse()
+            {
+                ShowID = await ShowID.GetAsync(new AmazonDynamoDBClient())
+            };
+        }
+    }
+}
